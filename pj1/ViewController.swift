13608b748dc7b96c84a05f6ac356f44773d66da9
choiKi <86941191+choiKi@users.noreply.github.com>
2022-09-09 15:35:07 +0900

    Initial Commit

---
+++ pj1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pj1
+//
+//  Created by 최기훈 on 2022/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
